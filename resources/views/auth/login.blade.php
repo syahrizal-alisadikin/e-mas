9936d6a52780686e6e8455a75c1a5effe0d6c594
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2021-12-18 14:20:44 +0700

    update admin transaction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,7 +87,7 @@
                             <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Login UMKM!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Login E-MAS!</h1>
                                     </div>
                                    <form method="POST" action="{{ route('login') }}" class="user">
                         @csrf
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -92,7 +92,7 @@
                             <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Register UMKM!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Register E-MAS!</h1>
                                     </div>
                           <form method="POST" action="{{ route('register') }}" class="user">
                         @csrf
